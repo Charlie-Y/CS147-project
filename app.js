13cf3fdd5769979ad30802080ccf1c1f9c1b562d
Charlie Yang <charlesy@stanford.edu>
2014-02-06 23:58:42 -0800

    also got tempting working out with conditional css file loading. js scripts are still page specific

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+// app.engine('handlebars', handlebars({'defaultLayout':'main'}));
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.use(express.favicon());
--- routes/sandbox.js
+++ routes/sandbox.js
@@ -5,6 +5,8 @@ exports.view = function(req, res){
     var videoId = req.params.videoId; 
     res.render('charlie_sandbox',
         {
+            'layout':'main',
+            'cssFiles': [{filename: 'charlie_sandbox.css'}],
             'videoId': videoId
         });
 
